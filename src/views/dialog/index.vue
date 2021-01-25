d454b834f76074ef11b39a0fe09799d70f5f1fe5
董鹏 <804974112@qq.com>
2021-01-25 18:05:46 +0800

    test

--- src/views/dialog/index.vue
+++ src/views/dialog/index.vue
@@ -51,8 +51,8 @@ export default class extends Vue {
   name = '';
   private formLabelWidth = '120px';
   selectAction(data: any, data1: any) {
-    this.actionDialogVisible = false;
-    this.region = this.form.region
+    this.actionDialogVisible = false
+    this.region = this.form.region;
     this.name = this.form.name;
   }
 }
